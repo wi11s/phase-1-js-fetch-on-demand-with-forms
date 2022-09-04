3d221e060c81ca2442f0e71ed4fa91d36882eea8
William <wms274@nyu.edu>
2022-09-04 02:25:57 -0400

    commit tree 91d4140e62a2214c9fd58685638f4dd325269f75

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+const inputForm = document.querySelector('form');
+document.addEventListener('submit', (e) => e.preventDefault())
